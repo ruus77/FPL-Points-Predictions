5feee4a4752f5950e634edcf5255d380348d16c7
ruus77 <russamuel2004@gmail.com>
2026-02-16 21:55:28 +0100

    helper functions: data import, data sorting

---
+++ Scripts/utils.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from pathlib import Path
+from typing import List
+
+def data_import(season_list: List[str] | None = None) -> pd.DataFrame:
+    if season_list is None:
+        return pd.DataFrame()
+
+    dfs = []
+
+    script_dir = Path(__file__).resolve().parent
+    project_root = script_dir.parent.parent
+    data_path = project_root / "Fantasy-Premier-League" / "data"
+
+    for season in season_list:
+        file_path = data_path / season / "gws" / "merged_gw.csv"
+
+        if not file_path.exists():
+            continue
+        try:
+            df = pd.read_csv(file_path, encoding='latin-1', low_memory=False)
+            df['season_id'] = season
+            dfs.append(df)
+
+        except pd.errors.EmptyDataError:
+            continue
+        except Exception as e:
+            print(f"Błąd przy sezonie {season}: {e}")
+
+    if not dfs:
+        return pd.DataFrame()
+
+    return pd.concat(dfs, axis=0, ignore_index=True)
+
+
+
+def sort_data(df:pd.DataFrame)->pd.DataFrame:
+  return df.sort_values(by=["name", "kickoff_time", "GW", "season_id"])
