a3867cdc28f9c39b7e00de0135481c0da7a5e592
ruus77 <russamuel2004@gmail.com>
2026-02-19 20:41:09 +0100

    added ModelSelector class to pick the best estimator

---
+++ Scripts/model_utils.py
@@ -0,0 +1,90 @@
+import numpy as np
+import sklearn
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import RandomizedSearchCV
+import pandas as pd
+
+class ModelSelector:
+
+    def __init__(self, random_state:int=77, scoring:str="mse"):
+      self.random_state = random_state
+      self.scoring = scoring
+
+    @staticmethod
+    def metrics_report(y_pred: np.ndarray, y_true: np.ndarray):
+        mse = mean_squared_error(y_true, y_pred)
+        mae = mean_absolute_error(y_true, y_pred)
+        r2 = r2_score(y_true, y_pred)
+
+        return mse, mae, r2
+
+    def params_search(self,
+                    models: list[sklearn.base.BaseEstimator],
+                    models_names: list[str],
+                    params_grid: list[dict[str, list[str]]],
+                    X_train: np.ndarray,
+                    y_train: np.ndarray,
+                    cv:int=5,
+                    scoring:str | None=None,
+                    n_iter: int = 20):
+
+        scoring = scoring if scoring else self.scoring
+
+        results_list = []
+        best_models_map = {}
+
+        for model, name, grid in zip(models, models_names, params_grid):
+            random_search = RandomizedSearchCV(cv=cv,
+                                               n_iter=n_iter,
+                                               estimator=model,
+                                               scoring=scoring,
+                                               param_distributions=grid,
+                                               verbose=1,
+                                               n_jobs=-1,
+                                               error_score="raise",
+                                               random_state=self.random_state,
+                                               refit=True)
+            random_search.fit(X_train, y_train)
+
+            best_models_map[name] = random_search.best_estimator_
+            cv_score = random_search.best_score_
+
+            y_train_pred = random_search.best_estimator_.predict(X_train)
+            metrics = self.metrics_report(y_true=y_train,
+                                        y_pred=y_train_pred)
+            row = {
+                "data": "train",
+                "name": name,
+                f"cv_mean_{scoring}" : cv_score,
+                "best_params": random_search.best_params_,
+                "mse": metrics[0],
+                "mae": metrics[1],
+                "r2": metrics[2]}
+
+            results_list.append(row)
+
+        return pd.DataFrame(results_list).sort_values(by=f"cv_mean_{scoring}", ascending=False), best_models_map
+
+    def evaluate(self, trained_models_map, X_test, y_test):
+        results_list = []
+        y_preds = {}
+
+        for name, model in trained_models_map.items():
+            y_pred = model.predict(X_test)
+            y_preds[name] = y_pred
+
+            metrics = self.metrics_report(y_pred=y_pred, y_true=y_test)
+
+            row = {
+                "data": "test",
+                "model_name": name,
+                "mse": metrics[0],
+                "mae": metrics[1],
+                "r2": metrics[2]}
+            results_list.append(row)
+
+        df_results = pd.DataFrame(results_list)
+
+        sort_column = self.scoring if self.scoring in df_results.columns else "mse"
+
+        return df_results.sort_values(by=sort_column, ascending=False), y_preds
