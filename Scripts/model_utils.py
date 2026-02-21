c3ec52669359a5587046e86b3f8523dbf6edf122
ruus77 <russamuel2004@gmail.com>
2026-02-21 16:52:33 +0100

    one_hot_encode fn + train_test_split fn

--- Scripts/model_utils.py
+++ Scripts/model_utils.py
@@ -3,6 +3,8 @@ import sklearn
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import RandomizedSearchCV, TimeSeriesSplit
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+
 
 class ModelSelector:
 
@@ -88,3 +90,34 @@ class ModelSelector:
         sort_column = self.scoring if self.scoring in df_results.columns else "mse"
 
         return df_results.sort_values(by=sort_column, ascending=False), y_preds
+
+    def train_test_split(df: pd.DataFrame) -> tuple:
+        df = df.copy()
+        X = df.drop(columns=["total_points"])
+        y = df["total_points"]
+        X_train, X_valid, X_test = X[X.season_id < "2024-25"], X[X.season_id == "2024-25"], X[X.season_id > "2024-25"]
+        y_train, y_valid, y_test = y[X.season_id < "2024-25"], y[X.season_id == "2024-25"], y[X.season_id > "2024-25"]
+        return (X_train, y_train,
+                X_valid, y_valid,
+                X_test, y_test)
+
+
+
+def one_hot_encode(X_train:pd.DataFrame,
+                   X_valid:pd.DataFrame,
+                   X_test:pd.DataFrame,
+                   cols_to_encode:list[str])->tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    encoder = OneHotEncoder(handle_unknown="ignore", sparse_output=False)
+    encoder.set_output(transform="pandas")
+
+    enc_train = encoder.fit_transform(X_train[cols_to_encode]).astype(int)
+    enc_valid = encoder.transform(X_valid[cols_to_encode]).astype(int)
+    enc_test = encoder.transform(X_test[cols_to_encode]).astype(int)
+
+    X_train, X_valid, X_test = (X_train.drop(columns=cols_to_encode),
+                                X_valid.drop(columns=cols_to_encode),
+                                X_test.drop(columns=cols_to_encode))
+
+    return (pd.concat([X_train, enc_train], axis=1).reset_index(drop=True).select_dtypes(include=np.number),
+            pd.concat([X_valid, enc_valid], axis=1).reset_index(drop=True).select_dtypes(include=np.number),
+            pd.concat([X_test, enc_test], axis=1).reset_index(drop=True).select_dtypes(include=np.number))
