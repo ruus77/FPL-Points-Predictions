64d9b54a5fa8855de178fc8f9987275a55330156
ruus77 <russamuel2004@gmail.com>
2026-02-22 15:59:56 +0100

    feat(trainer): implementacja klasy Trainer do obsługi cyklu uczenia w PyTorch
    - Dodano metody train_step i test_step realizujące cykl uczenia oraz ewaluacji (forward/backward pass).
    
    - Zintegrowano bibliotekę torchmetrics do precyzyjnego i zoptymalizowanego obliczania metryk regresji (MSE, MAE, R2).
    
    - Zaimplementowano pętlę treningową w metodzie model_eval ze śledzeniem postępów za pomocą biblioteki tqdm.
    
    - Dodano mechanizm Early Stopping z konfigurowalnymi parametrami patience i tolerance, chroniący przed przeuczeniem modelu.
    
    - Wprowadzono gromadzenie historii wyników z poszczególnych epok przy użyciu struktury defaultdict.

---
+++ Scripts/model_utils/trainer.py
@@ -0,0 +1,152 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from torchmetrics import MeanAbsoluteError, MeanSquaredError, R2Score
+from tqdm.auto import tqdm
+from collections import defaultdict
+
+
+class Trainer:
+    def __init__(self,
+                 device,
+                 random_state: int = 77):
+
+        torch.manual_seed(random_state)
+        np.random.seed(random_state)
+
+        self.device = device
+
+        self.mse = MeanSquaredError().to(device)
+        self.mae = MeanAbsoluteError().to(device)
+        self.r2 = R2Score().to(device)
+
+    def train_step(self,
+                   train_dataloader: torch.utils.data.DataLoader,
+                   model: nn.Module,
+                   optimizer: torch.optim.Optimizer,
+                   loss_fn: nn.Module):
+
+        train_loss = torch.tensor(0.0, device=self.device)
+
+        model = model.to(self.device)
+        model.train()
+
+        self.mse.reset()
+        self.mae.reset()
+        self.r2.reset()
+
+        for batch, (X_train, y_train) in enumerate(train_dataloader):
+            X_train, y_train = X_train.to(self.device), y_train.to(self.device)
+
+            optimizer.zero_grad()
+
+            y_pred = model(X_train)
+
+            loss = loss_fn(y_pred, y_train)
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.detach()
+
+            self.mse(y_pred, y_train)
+            self.mae(y_pred, y_train)
+            self.r2(y_pred, y_train)
+
+        train_mse = self.mse.compute().item()
+        train_mae = self.mae.compute().item()
+        train_r2 = self.r2.compute().item()
+
+        train_loss = train_loss.item() / len(train_dataloader)
+
+        return train_loss, train_mse, train_mae, train_r2
+
+    def test_step(self,
+                  test_dataloader: torch.utils.data.DataLoader,
+                  model: nn.Module,
+                  loss_fn: nn.Module):
+
+        test_loss = torch.tensor(0.0, device=self.device)
+        model = model.to(self.device)
+
+        self.mse.reset()
+        self.mae.reset()
+        self.r2.reset()
+
+        with torch.inference_mode():
+            model.eval()
+            for batch, (X_test, y_test) in enumerate(test_dataloader):
+                X_test, y_test = X_test.to(self.device), y_test.to(self.device)
+
+                y_pred = model(X_test)
+
+                loss = loss_fn(y_pred, y_test)
+                test_loss += loss.detach()
+
+                self.mse(y_pred, y_test)
+                self.mae(y_pred, y_test)
+                self.r2(y_pred, y_test)
+
+        test_mse = self.mse.compute().item()
+        test_mae = self.mae.compute().item()
+        test_r2 = self.r2.compute().item()
+
+        test_loss = test_loss.item() / len(test_dataloader)
+
+        return test_loss, test_mse, test_mae, test_r2
+
+    def model_eval(self,
+                   train_dataloader: torch.utils.data.DataLoader,
+                   test_dataloader: torch.utils.data.DataLoader,
+                   model: nn.Module,
+                   optimizer: torch.optim.Optimizer,
+                   loss_fn: nn.Module,
+                   num_epochs: int = 100,
+                   patience: int = 5,
+                   tolerance: float = 0.01):
+
+        best_loss = float("inf")
+        counter = 0
+
+        results = defaultdict(list)
+
+        for epoch in tqdm(range(num_epochs)):
+
+            train_loss, train_mse, train_mae, train_r2 = self.train_step(
+                train_dataloader=train_dataloader,
+                model=model,
+                optimizer=optimizer,
+                loss_fn=loss_fn
+            )
+
+            test_loss, test_mse, test_mae, test_r2 = self.test_step(
+                test_dataloader=test_dataloader,
+                model=model,
+                loss_fn=loss_fn
+            )
+
+            if test_loss < (best_loss - tolerance):
+                best_loss = test_loss
+                counter = 0
+            else:
+                counter += 1
+                if counter >= patience:
+                    print(f"Early stopping | No improvement since {patience} epochs")
+                    break
+
+            results["train_loss"].append(train_loss)
+            results["test_loss"].append(test_loss)
+            results["train_mse"].append(train_mse)
+            results["test_mse"].append(test_mse)
+            results["train_mae"].append(train_mae)
+            results["test_mae"].append(test_mae)
+            results["train_r2"].append(train_r2)
+            results["test_r2"].append(test_r2)
+
+            log_interval = max(1, int(num_epochs / 10))
+            if epoch % log_interval == 0:
+                print(f"Epoch: {epoch} | Train Loss: {train_loss:.4f} | Test Loss: {test_loss:.4f}\n"
+                      f"Train MSE: {train_mse:.4f} | Test MSE: {test_mse:.4f}\n"
+                      f"Train MAE: {train_mae:.4f} | Test MAE : {test_mae:.4f}\n"
+                      f"Train R2: {train_r2:.4f} | Test R2 : {test_r2:.4f}")
+
+        return results
