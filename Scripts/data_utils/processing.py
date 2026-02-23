98af54feaf631dbc3fa59839f3b99bb7fb84c4c2
ruus77 <russamuel2004@gmail.com>
2026-02-23 19:23:25 +0100

    sort_data correction

--- Scripts/data_utils/processing.py
+++ Scripts/data_utils/processing.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
 def sort_data(df: pd.DataFrame) -> pd.DataFrame:
-    return df.sort_values(by=["player_code", "seasson", "gw", "match_id"]).reset_index(drop=True)
+    return df.sort_values(by=["player_code", "season_id", "gw", "match_id"]).reset_index(drop=True)
