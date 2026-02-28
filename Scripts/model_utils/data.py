5dba49e381fb9eacc71d28343421fd0c843f1f16
ruus77 <russamuel2004@gmail.com>
2026-02-28 15:30:43 +0100

    added to_seq_tensor methor for rnn application

--- Scripts/model_utils/data.py
+++ Scripts/model_utils/data.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import torch
 from torch.utils.data import DataLoader, TensorDataset
@@ -11,8 +12,8 @@ def train_test_split(df: pd.DataFrame)->tuple[pd.DataFrame, pd.Series,
     df = df.copy()
     X = df.drop(columns=["event_points"])
     y = df["event_points"]
-    X_train, X_valid, X_test = X[X.season_id == 2425], X[(X.season_id == 2425) & (X.gw <= X.gw.max()//2)], X[(X.season_id == 2425) & (X.gw > X.gw.max()//2)]
-    y_train, y_valid, y_test = y[X.season_id == 2425], y[(X.season_id == 2425) & (X.gw <= X.gw.max()//2)], y[(X.season_id == 2425) & (X.gw > X.gw.max()//2)]
+    X_train, X_valid, X_test = X[X.season_id == 2425], X[(X.season_id == 2526) & (X.gw <= X.gw.max()//2)], X[(X.season_id == 2526) & (X.gw > X.gw.max()//2)]
+    y_train, y_valid, y_test = y[X.season_id == 2425], y[(X.season_id == 2526) & (X.gw <= X.gw.max()//2)], y[(X.season_id == 2526) & (X.gw > X.gw.max()//2)]
     return (X_train, y_train,
             X_valid, y_valid,
             X_test, y_test)
@@ -20,11 +21,15 @@ def train_test_split(df: pd.DataFrame)->tuple[pd.DataFrame, pd.Series,
 
 
 class FPLDataPipe:
-    def __init__(self, num_cols: list[str], cat_cols: list[str], batch_size: int = 64):
+    def __init__(self, num_cols: list[str], cat_cols: list[str], batch_size: int = 64, seq_length: int = 5):
         self.num_cols = num_cols
         self.cat_cols = cat_cols
+        self.seq_length = seq_length
         self.batch_size = batch_size
         self.preprocessor = self._build_pipeline()
+
+        self.feature_size = 0
+        self.train_dataloader, self.valid_dataloader, self.test_dataloader = None, None, None
 
     def _build_pipeline(self):
         num_transform = Pipeline([
@@ -37,31 +42,48 @@ class FPLDataPipe:
 
         preprocessor = ColumnTransformer([
             ("num", num_transform, self.num_cols),
-            ("col", cols_transform, self.cat_cols)],
-            remainder="drop")
-
+            ("cat", cols_transform, self.cat_cols),
+            ("passthrough", "passthrough", ["player_code", "season_id"])
+        ],
+            remainder="drop",
+            verbose_feature_names_out=False
+        )
         preprocessor.set_output(transform="pandas")
         return preprocessor
 
-    def _to_tensor(self, X, y):
-        return (
-            torch.tensor(X.to_numpy(), dtype=torch.float32),
-            torch.tensor(y.to_numpy().reshape(-1, 1), dtype=torch.float32)
-        )
+    def _to_seq_tensor(self, X, y):
+        X_seq, y_seq = [], []
+        X_features = X.drop(columns=["player_code", "season_id"])
+
+        for _, group in X.groupby(["player_code", "season_id"]):
+            if len(group) <= self.seq_length:
+                continue
+
+            feat_vals = X_features.loc[group.index].values
+            target_vals = y.loc[group.index].values
+
+            for i in range(len(group) - self.seq_length):
+                X_seq.append(feat_vals[i : i + self.seq_length])
+                y_seq.append(target_vals[i + self.seq_length])
+
+        X_tensor = torch.tensor(np.array(X_seq), dtype=torch.float32)
+        y_tensor = torch.tensor(np.array(y_seq), dtype=torch.float32).reshape(-1, 1)
+        return X_tensor, y_tensor
 
     def prepare_data(self, X_train, X_valid, X_test, y_train, y_valid, y_test):
         X_train = self.preprocessor.fit_transform(X_train)
         X_valid = self.preprocessor.transform(X_valid)
         X_test = self.preprocessor.transform(X_test)
+        self.feature_size = X_train.shape[1] - 2
 
-        X_train_tensor, y_train_tensor = self._to_tensor(X_train, y_train)
-        X_valid_tensor, y_valid_tensor = self._to_tensor(X_valid, y_valid)
-        X_test_tensor, y_test_tensor = self._to_tensor(X_test, y_test)
+        X_train_tensor, y_train_tensor = self._to_seq_tensor(X_train, y_train)
+        X_valid_tensor, y_valid_tensor = self._to_seq_tensor(X_valid, y_valid)
+        X_test_tensor, y_test_tensor = self._to_seq_tensor(X_test, y_test)
 
         self.train_dataloader = DataLoader(
             TensorDataset(X_train_tensor, y_train_tensor),
             batch_size=self.batch_size,
-            shuffle=False
+            shuffle=True
         )
         self.valid_dataloader = DataLoader(
             TensorDataset(X_valid_tensor, y_valid_tensor),
