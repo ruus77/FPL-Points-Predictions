610c097b7f06cb527d6b4c028ac80e1ee306c939
ruus77 <russamuel2004@gmail.com>
2026-03-09 11:31:30 +0100

    added new functions

--- Scripts/data_utils/__init__.py
+++ Scripts/data_utils/__init__.py
@@ -1,5 +1,5 @@
 from .loaders import data_import, club_data_import
-from .processing import sort_data
+from .processing import sort_data, id_cols_detection, duplicated_cols
 from .analysis import table_simulation
 from .visualization import best_players_plots, expected_stats_vs_actuals
 from .colors_config import team_colors
