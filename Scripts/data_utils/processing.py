36fa483068243e8b7336ff5cac69503f86dbbb8d
ruus77 <russamuel2004@gmail.com>
2026-03-09 17:58:59 +0100

    corrected fns

--- Scripts/data_utils/processing.py
+++ Scripts/data_utils/processing.py
@@ -1,22 +1,22 @@
 import pandas as pd
 
-def sort_data(df: pd.DataFrame) -> pd.DataFrame:
-    return df.sort_values(by=["player_code", "season_id", "gw", "match_id"]).reset_index(drop=True)
-
-
 def id_cols_detection(df: pd.DataFrame) -> pd.DataFrame:
     id_cols = [c for c in df.columns if (
-            ("_id" in c or "id_" in c or c == "id")
-            or "code" in c
+            "code" in c
             or "name" in c
             or "position" in c
             or 'status' in c
+            or  c == "season"
             or df[c].dtype == 'object'
             or c == "gw"
-    ) or c in ["season_id", "gw"]]
-    ic_cols = [f"{c}_id" for c in id_cols if "id" not in c]
-    return df[id_cols + [c for c in df.columns if c not in id_cols]]
+     or c == "gw") if c not in ["touches_opposition_box", "offsides"]]
 
+    new_id_cols = [f"{c}_id" for c in id_cols]
+    df.rename(columns=dict(zip(id_cols, new_id_cols)), inplace=True)
+    return df[new_id_cols + [c for c in df.columns if c not in id_cols and c not in new_id_cols]]
+
+def sort_data(df: pd.DataFrame) -> pd.DataFrame:
+    return df.sort_values(by=["player_code_id", "season_id", "gw_id", "match_id"]).reset_index(drop=True)
 
 
 def duplicated_cols(df:pd.DataFrame)->list[str]:
