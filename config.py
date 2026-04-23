30338122fbcb7f31994ed669f2e3b55adc11e553
Samuel Rus <148883293+ruus77@users.noreply.github.com>
2026-04-23 18:48:06 +0200

    Simplify config.py and update data paths
    Refactor paths and remove redundant code.

--- config.py
+++ config.py
@@ -1,26 +1,16 @@
 from pathlib import Path
 import sys
 
-BASE_DIR = Path(__file__).resolve().parent.parent
-DATA_PATH = BASE_DIR / 'Data' / 'FPL_data.parquet'
-
-SCRIPTS_PATH = BASE_DIR / 'Scripts'
-if str(SCRIPTS_PATH) not in sys.path:from pathlib import Path
-import sys
-
-BASE_DIR = Path(r'C:\Users\russa\PycharmProjects\FPL_Points_Predicting\FPL-Points-Predictions')
+BASE_DIR = Path(__file__).resolve().parent
 
 DATA_DIR = BASE_DIR / 'Data'
 SCRIPTS_PATH = BASE_DIR / 'Scripts'
 
-DATA_PATH = DATA_DIR / 'FPL_data.parquet'
+FPL_DATA_PATH = DATA_DIR / 'FPL_data.parquet'
+TIDY_DATA_PATH = DATA_DIR / 'FPL_data_tidy.parquet'
 UNTIDY_DATA_PATH = DATA_DIR / 'FPL_data_untidy.parquet'
+
+PREDICTED_STATS_PATH = DATA_DIR / 'player_data.parquet'
 
 if str(SCRIPTS_PATH) not in sys.path:
     sys.path.append(str(SCRIPTS_PATH))
-    sys.path.append(str(SCRIPTS_PATH))
-
-
-TIDY_DATA_PATH = DATA_DIR / 'FPL_data_tidy.parquet'
-
-FPL_DATA_PATH = DATA_DIR / 'FPL_data.parquet'
